3f97cded4f46e248944bf47895eeec03fb148f59
RodrigoBS56 <rodrigobarbosadesousa5608@gmail.com>
2025-06-25 16:39:23 -0400

    atualiza a o arquivo contato.html, css e js

--- src/main/resources/static/js/navbar.js
+++ src/main/resources/static/js/navbar.js
@@ -1,11 +1,9 @@
-// Ativação do burger menu no mobile
 document.addEventListener('DOMContentLoaded', () => {
-    const burger = document.querySelector('.navbar-burger');
-    const menu = document.querySelector('#navbarMenu');
-    if (burger && menu) {
-        burger.addEventListener('click', () => {
-            burger.classList.toggle('is-active');
-            menu.classList.toggle('is-active');
-        });
-    }
+  const burger = document.querySelector('.navbar-burger');
+  const menu = document.getElementById(burger.dataset.target);
+
+  burger.addEventListener('click', () => {
+    burger.classList.toggle('is-active');
+    menu.classList.toggle('is-active');
+  });
 });
